65ffed61ce076c04563a8b0d040f47a394839f4e
start.vaadin.com <no-reply@vaadin.com>
2026-08-17 04:51:30 +0000

    Generated project

---
+++ src/main/java/com/isateca/Application.java
@@ -0,0 +1,22 @@
+package com.isateca;
+
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+
+@SpringBootApplication
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet(Lumo.UTILITY_STYLESHEET)
+@StyleSheet("styles.css") // Your custom styles
+@Push
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/isateca/base/ui/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.isateca.base.ui;
+
+import org.jspecify.annotations.NullMarked;
